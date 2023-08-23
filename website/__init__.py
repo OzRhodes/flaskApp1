d5d31b9ec04e56c40bba1fb5651afa5b26b15291
oz rhodes <oz@ozrhodes.com>
2023-08-23 12:59:59 +0100

    update init.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
+
+
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'A secret key but will be removed on a production'
+    
+    # config database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    # models are in models.py
 
     from .views import views
     from .auth import auth
@@ -13,6 +24,8 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
 
     app.register_blueprint(auth, url_prefix='/')
+
+    import .models 
 
 
     return app
