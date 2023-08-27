6faa7d93349616b925f72c5f8faa00775063e7ea
oz rhodes <oz@ozrhodes.com>
2023-08-27 10:06:06 +0100

    Added SQLite

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,11 @@ from flask import Blueprint
 from flask import render_template
 from flask import request
 from flask import flash
+from flask import redirect
+from flask import url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 # routes are here
 
@@ -9,10 +14,19 @@ auth = Blueprint('auth',__name__)
 
 @auth.route('/login', methods = ['GET','POST'])
 
+
 def login():
-    data = request.form
-    
-    return render_template ('login.html', data = data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+
+
+    return render_template("login.html")
 
 @auth.route('/logout')
 
@@ -27,8 +41,13 @@ def register():
         name = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+    # Check no user already exists with this email
+        user = User.query.filter_by(email = email).first()
+        if user:
+            flash('User already exists', category = 'error')
+
     # Check Valid
-        if len(email)<4:
+        elif len(email)<4:
             flash('Email must be a real email address greater than 4 characters', category = 'error')
         elif len(name)<2:
             flash('Name must be greater than 1 characters', category = 'error')
@@ -37,6 +56,11 @@ def register():
         elif len(password1) <5:
             flash('Password must greater than 4 characters', category = 'error')
         else:
-            flash('Account Created', category = 'success')
-            #add to database
+            new_user = User(email=email, name=name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+         #   login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
     return render_template ('register.html')
--- website/models.py
+++ website/models.py
@@ -2,19 +2,18 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class User(db.Model, UserMixin):
-    # Define the schema
-    id = db.Column(db.Integer, primary_key = True)  #primary key
-    email = db.Column(db.String(150), unique = True)
-    password = db.Column(db.String(150))
-    name = db.Column(db.String(50))
-
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    data = db.Column(db.String(15000))
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    # foreign key association/relation
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # one to many 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    name = db.Column(db.String(150))
     notes = db.relationship('Note')
 
 
